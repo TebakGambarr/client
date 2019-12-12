0235c74c7fd30bd1fc64daea352783a512bbdbcf
memorvin <alvina.tjia@yahoo.com>
2019-12-12 18:21:13 +0700

    adding play arena skeleton

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChatBox',
+  components: {
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <!-- <draw /> -->
+  <chat-box />
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import Draw from '@/components/Draw.vue'
+import ChatBox from '@/components/ChatBox.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ChatBox
+    // Draw
   }
 }
 </script>
